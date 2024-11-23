95b7c78c7c0ffd364fdbe62bc7c47a189e964bcb
Benjamin Chuunga <benjamin.chuunga@gmail.com>
2024-11-23 18:41:10 -0500

    Adding Authentication

---
+++ clever/src/app/components/layout/footer-minimal/footer-minimal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clever-footer-minimal',
+  templateUrl: './footer-minimal.component.html',
+  styleUrls: ['./footer-minimal.component.scss']
+})
+export class FooterMinimalComponent {
+
+}
