de79e9793d55348829aa7747f43a97d26e51534d
Benjamin Chuunga <benjamin.chuunga@gmail.com>
2024-11-23 21:10:35 -0500

    Updating Files

---
+++ clever/src/app/components/layout/logo-minimal/logo-minimal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clever-logo-minimal',
+  templateUrl: './logo-minimal.component.html',
+  styleUrls: ['./logo-minimal.component.scss']
+})
+export class LogoMinimalComponent {
+
+}
